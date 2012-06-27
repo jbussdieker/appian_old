58a71134e7c71a9939bfe980750c1972a597f2fd
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-06-27 13:20:26 -0700

    Add user name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627035346) do
+ActiveRecord::Schema.define(:version => 20120627201616) do
 
   create_table "keys", :force => true do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20120627035346) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
