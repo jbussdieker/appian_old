c1992923b68506bd2fdad316ffc53adc41d0c30d
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-06-26 18:32:09 -0700

    Sweet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slit::Application.config.secret_token = 'fec23f27bc94f401c78c223090129479b9124f057f53fe96cee2b9e27db60f4bcb1d183e0e0fc2cf3d3e01763345d479a6ac90dfdaccfcada9eddb531f19a0f9'
