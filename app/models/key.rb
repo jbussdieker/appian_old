35f995181208d1fe22b1420387a569073d64fffa
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-17 23:15:46 -0700

    Better use of rails

--- app/models/key.rb
+++ app/models/key.rb
@@ -4,6 +4,20 @@ class Key < ActiveRecord::Base
 
   belongs_to :user
 
+  after_destroy :write_authorized_keys
+  after_save :write_authorized_keys
+
+  def write_authorized_keys
+    data = ""
+    Key.all.each do |key|
+      data << "command=\"#{File.join(Rails.root, "script", "git")} #{user.email}\""
+      data << ",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty "
+      data << "#{key.key}\n"
+    end
+    keyfile = File.join(Rails.configuration.sshdir, "authorized_keys")
+    File.open(keyfile, 'w') {|f| f.write(data) }
+  end
+
   def key_is_valid
     begin
        Net::SSH::KeyFactory.load_data_public_key(key)
