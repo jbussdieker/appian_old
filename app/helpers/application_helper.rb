5141255c8cfcb9b497a0b3dfc599f8859a4f812a
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-19 01:06:51 -0700

    Split up signin form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,16 @@ module ApplicationHelper
       content_tag(:i, "", class: "icon-user")
     end
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
