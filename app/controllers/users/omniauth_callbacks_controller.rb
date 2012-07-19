657a1fbc2d234f874b13d0228c422019ed8157cc
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-18 20:45:15 -0700

    Debugtastic!

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def method_missing(provider)
     if !User.omniauth_providers.index(provider).nil?
       omniauth = env["omniauth.auth"]
+      logger.debug env
+      logger.debug "OmniAuth Info: Params: #{params} Omni.Auth: #{omniauth}"
 
       if current_user
         current_user.user_tokens.find_or_create_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
@@ -13,7 +15,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
           sign_in_and_redirect(:user, authentication.user)
         else
-          render :text => "Unknown user: #{params} OMNI: #{omniauth}"
+          unless omniauth.recursive_find_by_key("email").blank?
+            user = User.find_or_initialize_by_email(:email => omniauth.recursive_find_by_key("email"))
+          else
+            user = User.new
+          end
+
+          user.apply_omniauth(omniauth)
+
+          if user.save
+            flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => omniauth['provider']
+            sign_in_and_redirect(:user, user)
+          else
+            session[:omniauth] = omniauth.except('extra')
+            redirect_to new_user_registration_url
+          end
         end
       end
     else
