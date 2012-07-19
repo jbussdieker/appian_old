0ad72ace869a645f35097548dbea0c698c8becf3
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-18 19:57:38 -0700

    Try new omniauth stuff

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    render :text => "ASDF #{params}"
+    if !User.omniauth_providers.index(provider).nil?
+      omniauth = env["omniauth.auth"]
+      render :text => "ASDF #{params} OMNI: #{omniauth}"
+    else
+      render :text => "NOPS ASDF #{params}"
+    end
   end
 end
