5fce12084c1d3f33d9956a2eb1eaae9cd7adf61b
Joshua Bussdieker <jbussdieker@gmail.com>
2012-07-19 22:59:40 -0700

    Update job build badge

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -3,14 +3,20 @@ module JobsHelper
     begin
       color = job.build_info["color"]
     rescue Exception => e
-      return
+      content_tag(:i, "", class: "badge badge-warning")
     end
     if color == "grey"
-      content_tag(:i, "", class: "badge")
+      content_tag(:i, "idle", class: "badge")
     elsif color == "red"
-      content_tag(:i, "", class: "badge badge-important")
+      content_tag(:i, "idle", class: "badge badge-important")
     elsif color == "blue"
-      content_tag(:i, "", class: "badge badge-success")
+      content_tag(:i, "idle", class: "badge badge-success")
+    elsif color == "red_anime"
+      content_tag(:i, "running", class: "badge badge-important")
+    elsif color == "blue_anime"
+      content_tag(:i, "running", class: "badge badge-important")
+    else
+      content_tag(:i, "#{color}", class: "badge badge-success")
     end
   end
 end
