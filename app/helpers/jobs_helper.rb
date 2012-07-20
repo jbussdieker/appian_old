ad0e5ac1ecb35e8f4ae9b436bebac2d1727a8f40
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-19 21:26:59 -0700

    Build job status

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,12 @@
 module JobsHelper
+  def build_status_badge(job)
+    color = job.build_info["color"]
+    if color == "grey"
+      content_tag(:i, "", class: "badge")
+    elsif color == "red"
+      content_tag(:i, "", class: "badge badge-important")
+    elsif color == "blue"
+      content_tag(:i, "", class: "badge badge-success")
+    end
+  end
 end
