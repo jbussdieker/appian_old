a6359ee05288cd565268b00c52f122369d005c3f
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-21 17:22:50 -0700

    Fixes to job helper.

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -3,49 +3,52 @@ module JobsHelper
     begin
       color = job.build_info["color"]
     rescue Exception => e
-      content_tag(:i, "", class: "badge badge-warning")
+      return content_tag(:i, "offline", class: "badge badge-warning")
     end
     if color == "grey"
-      content_tag(:i, "idle", class: "badge")
+      content_tag(:i, "N/A", class: "badge")
     elsif color == "red"
-      content_tag(:i, "idle", class: "badge badge-important")
+      content_tag(:i, "failed", class: "badge badge-important")
     elsif color == "blue"
-      content_tag(:i, "idle", class: "badge badge-success")
+      content_tag(:i, "stable", class: "badge badge-success")
     elsif color == "red_anime"
       content_tag(:i, "running", class: "badge badge-important")
     elsif color == "blue_anime"
-      content_tag(:i, "running", class: "badge badge-important")
+      content_tag(:i, "running", class: "badge badge-success")
     else
       content_tag(:i, "#{color}", class: "badge badge-success")
     end
   end
 
   def last_failure(job)
+    return "-" if !job.build_info.kind_of? Hash
     last_build = job.build_info["lastFailedBuild"]
-    return if !last_build
+    return "N/A"  if !last_build
     last_build_num = last_build["number"]
     last_details = job.build_details(last_build_num)
-    return if !last_details
+    return "N/A"  if !last_details
     td = timediff(Time.at(last_details["timestamp"].to_i / 1000), :short)
     "#{td} (##{last_build_num})"
   end
 
   def last_success(job)
+    return "-" if !job.build_info.kind_of? Hash
     last_build = job.build_info["lastSuccessfulBuild"]
-    return if !last_build
+    return "N/A" if !last_build
     last_build_num = last_build["number"]
     last_details = job.build_details(last_build_num)
-    return if !last_details
+    return "N/A"  if !last_details
     td = timediff(Time.at(last_details["timestamp"].to_i / 1000), :short)
     "#{td} (##{last_build_num})"
   end
 
   def last_build_duration(job)
+    return "-" if !job.build_info.kind_of? Hash
     last_build = job.build_info["lastBuild"]
-    return if !last_build
+    return "N/A"  if !last_build
     last_build_num = last_build["number"]
     last_details = job.build_details(last_build_num)
-    return if !last_details
+    return "N/A"  if !last_details
     "#{last_details["duration"]} ms"
   end
 end
