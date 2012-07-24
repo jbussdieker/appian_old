07028f5087e6ed57ecdca1c5daa9e458433c38e2
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-23 22:08:16 -0700

    Add a home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to repositories_path
-    else
+    if !user_signed_in?
       redirect_to new_user_session_path
     end
+
+    @users = User.all
   end
 end
