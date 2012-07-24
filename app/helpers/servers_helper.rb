f8bdcdff192a4e264e6b94df27a506a966cd9120
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-23 23:14:56 -0700

    More helpers

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -7,8 +7,14 @@ module ServersHelper
     end
     if status == :stopped
       content_tag(:i, status, class: "badge")
+    elsif status == :pending
+      content_tag(:i, status, class: "badge")
     elsif status == :running
       content_tag(:i, status, class: "badge badge-success")
+    elsif status == :shutting_down
+      content_tag(:i, status, class: "badge badge-warning")
+    elsif status == :terminated
+      content_tag(:i, status, class: "badge")
     else
       content_tag(:i, "Unknown: #{status}", class: "badge")
     end
